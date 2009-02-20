e4ddb09c51cde6db634a464b8d300a74f26af3e1
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-02-20 11:52:40 +0000

    Added initial code

--- flserver/flserver.el
+++ flserver/flserver.el
@@ -1,1 +1,19 @@
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration t)
+
+(require 'htmlize)
 ;; http://www.emacswiki.org/emacs/download/htmlize.el
+
+(require 'server)
+(setq server-raise-frame  nil)
+(setq server-socket-dir   "~/")
+(setq server-name         ".flserver")
+
+(defun flserver-htmlize (input output-dir)
+  (htmlize-file input output-dir)
+  )
+(server-start)
+
+(while t
+  (sit-for 3600))
+(provide 'flserver)
