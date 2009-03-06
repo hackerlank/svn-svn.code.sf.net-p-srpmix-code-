8e6aa149547979b06712140c4d26b71ff604bb5b
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-03-06 09:34:56 +0000

    implement etags+srpmix-guess.el

---
+++ srpmix/trunk/utils/etags+srpmix-guess.el
@@ -0,0 +1,75 @@
+;;; etags+srpmmix-guess.el --- Guessing the place where plugin/etags/TAGS file is
+
+;; Copyright (C) 2009 Masatake YAMATO
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;; Commentary:
+;;
+;; In Sources environment, TAGS files are stored for somewhere special.
+;; 
+;; $ ls
+;; archives  plugins  pre-build  specs.spec  SRPMIX  STATUS
+;; $ ls plugins/
+;; etags
+;; $ ls plugins/etags
+;; TAGS
+;; $  
+;;
+;; Emacs with this elisp program will look for plugins/etags when
+;; you invoke `visit-tags-table' in somewhere under archives or 
+;; pre-build.
+;;
+
+;; INSTALL:
+;; (require 'etags+srpmix-guess)
+
+(require 'etags)
+
+(defvar stitch-original-visit-tags-table nil)
+(unless (fboundp 'stitch-original-visit-tags-table)
+  (fset 'stitch-original-visit-tags-table
+	(symbol-function 'visit-tags-table)))
+
+(defun stitch-search-tags-file (base)
+  (unless (equal base "/")
+    (let ((upper (file-name-directory base)))
+      (cond
+       ((not upper)
+	nil)
+       ;; srpmix
+       ((file-exists-p (concat upper "plugins/etags/TAGS"))
+	(concat upper "plugins/etags"))
+       ;; lcopy
+       ((file-exists-p (concat upper ".lcopy/TAGS"))
+	(concat upper ".lcopy"))
+       ;; lcopy NG
+       ((file-exists-p (concat upper ".lcopy/plugins/TAGS"))
+	(concat upper ".lcopy/plugins"))
+       (t
+	(stitch-search-tags-file (directory-file-name upper)))))))
+    
+(defun stitch-visit-tags-table (file &optional local)
+  (interactive (list nil nil))
+  (if file
+      (stitch-original-visit-tags-table file)
+    (let ((in-stitch (stitch-search-tags-file default-directory)))
+      (let ((default-directory (or in-stitch default-directory)))
+	(call-interactively 'stitch-original-visit-tags-table)))))
+
+(fset 'visit-tags-table (symbol-function 'stitch-visit-tags-table))
+
+(provide 'etags+srpmix-guess)
+;; etags+srpmix-guess.el ends here
