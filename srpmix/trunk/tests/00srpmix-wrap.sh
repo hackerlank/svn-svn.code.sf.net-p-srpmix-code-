842b81a6b77697505cc98a2c97d9de324bbaf252
taru <taru@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-03-14 06:40:03 +0000

    userland support

--- srpmix/trunk/tests/00srpmix-wrap.sh
+++ srpmix/trunk/tests/00srpmix-wrap.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -e
 
+if test -z "$TEST_SRPMS"; then
+    echo ";;; No TEST_SRPMS is specified" 1>&2
+# magic number defined in automake:
+#
+#   If a given test program exits with a status of 77,
+#   then its result is ignored in the final count.
+#
+    exit 77
+fi
+
 rm -rf `pwd`/${TESTDIR}
 make DESTDIR=`pwd`/${TESTDIR} -C ${top_builddir} install
 
--- srpmix/trunk/tests/00srpmix.sh
+++ srpmix/trunk/tests/00srpmix.sh
@@ -7,6 +7,7 @@ if test -z "$TEST_SRPMS"; then
 #   If a given test program exits with a status of 77,
 #   then its result is ignored in the final count.
 #
+#   TODO: --name=yum handling
     exit 77
 fi
 
@@ -18,6 +19,17 @@ PATH=`pwd`/${TESTDIR}/${bindir}:$PATH
 for srpm in $TEST_SRPMS
 do
   srpmix --output-dir=${TESTDIR} --output-format=swrf --srpm=$srpm
+  srpmix --output-format=swrf --srpm=$srpm
 done
+#TODO: test by configure?
+#srpmix --output-format=swrf --name=yum
 find ${TESTDIR} -name '*.swrf' | xargs -n1 --verbose rpm -qpl
+
+for srpm in $TEST_SRPMS
+do
+  srpmix --output-dir=${TESTDIR} --output-format=rpm --srpm=$srpm
+done
+#TODO: test by configure?
+#srpmix --output-dir=${TESTDIR} --output-format=rpm --name=yum
+find ${TESTDIR} -name '*.rpm' | xargs -n1 --verbose rpm -qpl
 
