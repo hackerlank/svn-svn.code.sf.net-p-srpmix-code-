9ae84a303466bcbc1fc3cc952206df711ba730df
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-03-18 07:38:30 +0000

    Sort the rule files before giving them to build-opt-rules.

---
+++ srpmix/trunk/src/helpers/99default.scm
@@ -0,0 +1,14 @@
+;; specs file are seriously broken.
+(#/kernel-2\.4\.18-e\.[1-6].*/ "--target=ia64")
+
+;; ia64 and ppc64 should be built, However, with these
+;; targets, created swrfs becomes too large.
+(#/kernel-2\.6\.18.*/ "--target=x86_64,i686"  )
+
+;;
+(#/kernel-2\.4\.9.*/  "--target=i686" )
+
+;;
+(#/yaboot-/ "--target=ppc64")
+
+
