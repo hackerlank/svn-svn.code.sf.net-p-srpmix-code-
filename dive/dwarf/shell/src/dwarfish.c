e52fc7545c6d47a5205946c7e17ccece8451d0d1
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-04-10 01:53:11 +0000

    updated

--- dive/dwarf/shell/src/dwarfish.c
+++ dive/dwarf/shell/src/dwarfish.c
@@ -1,24 +1,40 @@
 #include "dwarfish.h"
 #include "es-lang-c-stdc99.h"
 #include <stdlib.h>
+#include <argp.h>
 
 
+#define O(X) es_object_autounref(X) 
+#define UNDEF es_symbol_intern("undef")
 
-int dwarfish_variable_do    (const struct variable *var,
-			     struct cu *cu,
-			     DwarfishContext * ctx);
-int dwarfish_function_do    (const struct function * func, 
-			     struct cu *cu,
-			     DwarfishContext * ctx);
-int dwarfish_base_type_do   (struct base_type* base_type,
-			     struct cu *cu,
-			     DwarfishContext * ctx);
+
+typedef struct _TagHandlerTable TagHandlerTable;
+struct _TagHandlerTable {
+  int tag;
+  EsObject* (* convert_to_es) (const struct tag* tag,
+			       struct  cu*       cu,
+			       DwarfishContext*  ctx);
+}; 
+
+static int  dwarfish_cu_iterator    (struct cu   *cu, 
+				     void* cookie);
+static int  dwarfish_tag_iterator   (struct tag *tag, 
+				     struct cu *cu, 
+				     void* cookie);
 
 DwarfishContext *
 dwarfish_context_new  (void)
 {
   return malloc(sizeof(DwarfishContext));
 }
+
+void
+dwarfish_context_do   (DwarfishContext * ctx,
+		       struct cus *cus)
+{
+  cus__for_each_cu(cus, dwarfish_cu_iterator, ctx, NULL);
+}
+
 void
 dwarfish_context_free (DwarfishContext * fish)
 {
@@ -26,103 +42,267 @@ dwarfish_context_free (DwarfishContext * fish)
 }
 
 
+EsObject* dwarfish_variable_convert_to_es  (const struct tag* tag,
+				 struct cu *cu,
+				 DwarfishContext * ctx);
+EsObject* dwarfish_function_convert_to_es  (const struct tag* tag,
+				 struct cu *cu,
+				 DwarfishContext * ctx);
+EsObject* dwarfish_base_type_convert_to_es (const struct tag* tag,
+				 struct cu *cu,
+				 DwarfishContext * ctx);
+EsObject* dwarfish_typedef_convert_to_es   (const struct tag* tag,
+				 struct cu *cu,
+				 DwarfishContext * ctx);
+
+
+static TagHandlerTable tag_handler_table [] = 
+  {
+    { DW_TAG_variable,   dwarfish_variable_convert_to_es  },
+    { DW_TAG_subprogram, dwarfish_function_convert_to_es  },
+    { DW_TAG_base_type,  dwarfish_base_type_convert_to_es },
+    { DW_TAG_typedef,    dwarfish_typedef_convert_to_es   },
+    { 0,                 NULL                  },
+  };
+
+
 int
 dwarfish_tag_do       (struct tag *tag, 
 		       struct cu *cu, 
 		       DwarfishContext * ctx)
 {
-  if (tag->tag == DW_TAG_variable)
-    return dwarfish_variable_do(tag__variable(tag), cu, ctx);
-  else if (tag->tag == DW_TAG_subprogram)
-    return dwarfish_function_do(tag__function(tag), cu, ctx);
-  else if (tag->tag == DW_TAG_base_type)
-    return dwarfish_base_type_do(tag__base_type(tag), cu, ctx);
+  int i;
+  EsObject* o = UNDEF;
+
+
+
+  for (i = 0; ; ++i)
+    {
+      if (tag_handler_table[i].tag == 0
+	  && tag_handler_table[i].convert_to_es == NULL)
+	break;
+      else if (tag_handler_table[i].tag == tag->tag)
+	{
+	  es_autounref_pool_push();
+	  o = tag_handler_table[i].convert_to_es(tag, cu, ctx);
+	  es_autounref_pool_pop();
+	  break;
+	}
+    }
+
+  if (o != UNDEF)
+    {
+      es_print(o, NULL);
+      es_object_unref(o);
+    }
   else
     {
       const char* name;
-
+      
       name = dwarf_tag_name(tag->tag);
-      printf(";; TODO: %s\n", name);
-      return 0;
+      printf(";; TODO: %s", name);
     }
+
+  puts("");
+
+
+  return 0;
 }
 
 
 
-int
-dwarfish_variable_do  (const struct variable *var, 
+EsObject*
+dwarfish_variable_convert_to_es  (const struct tag* tag,
 		       struct cu *cu,
 		       DwarfishContext * fish)
 {
+  const struct variable *var;
   EsObject* o;
   const char* name;
 
+
+  var = tag__variable(tag);
+
+
+  o = UNDEF;
   name = variable__name(var, cu);
   if (!name)
     goto out;
 
-  o = es_srealize("(dwarfish variable :name %s :file %s :line %d)", 
-		  name,
-		  var->tag.decl_file,
-		  var->tag.decl_line
-		  );
-  es_print(o, NULL);
-  puts("");
-  es_object_unref(o);
+  o = es_srealize
+    (
+     "(dwarfish variable :name %s :file %s :line %d)", 
+     name,
+     var->tag.decl_file,
+     var->tag.decl_line
+     );
 
  out:
-  return 0;
+  return o;
 }
 
-int
-dwarfish_function_do  (const struct function * func, 
+EsObject*
+dwarfish_function_convert_to_es  (const struct tag* tag,
 		       struct cu *cu,
 		       DwarfishContext * fish)
 {
+  struct function * func;
   EsObject* o;
   const char* name;
 
 
+  func = tag__function(tag);
+
+
+  o = UNDEF;
   name = function__name(func, cu);
   if (!name)
     goto out;
 
-  o = es_srealize("(dwarfish function :name %s :file %s :line %d)", 
-		  name,
-		  func->proto.tag.decl_file? func->proto.tag.decl_file: (func->lexblock.tag.decl_file? func->lexblock.tag.decl_file: ""),
-		  func->proto.tag.decl_line? func->proto.tag.decl_line: (func->lexblock.tag.decl_line? func->lexblock.tag.decl_line: 0)
-		  );
-  es_print(o, NULL);
-  puts("");
-  es_object_unref(o);
+  o = es_srealize
+    (
+     "(dwarfish function :name %s :file %s :line %d)", 
+     name,
+     func->proto.tag.decl_file? func->proto.tag.decl_file: (func->lexblock.tag.decl_file? func->lexblock.tag.decl_file: ""),
+     func->proto.tag.decl_line? func->proto.tag.decl_line: (func->lexblock.tag.decl_line? func->lexblock.tag.decl_line: 0)
+     );
 
  out:  
-  return 0;
+  return o;
 }
   
 
-int
-dwarfish_base_type_do   (struct base_type* base_type,
+EsObject*
+dwarfish_base_type_convert_to_es   (const struct tag* tag,
 			 struct cu *cu,
 			 DwarfishContext * ctx)
 {
-  EsObject* o, * o0;
+  struct base_type* base_type;
+  EsObject* o;
   const char* name;
 
+
+  base_type = tag__base_type(tag);
+
+
+  o = UNDEF;
   name = base_type->name;
   if (!name)
     goto out;
 
-  o = es_srealize("(dwarfish base-type :name %s :size  %d :file %, :line %d)",
-		  name,
-		  base_type->size,
-		  o0 = (base_type->tag.decl_file? es_string_new(base_type->tag.decl_file): es_false) ,
-		  base_type->tag.decl_line);
-  es_print(o, NULL);
-  puts("");
-  es_object_unref(o);
-  es_object_unref(o0);
+  o = es_srealize
+    (
+     "(dwarfish base-type :name %s :size  %d :file %, :line %d)",
+     name,
+     tag__size(&base_type->tag, cu),
+     O(base_type->tag.decl_file? es_string_new(base_type->tag.decl_file): es_false) ,
+     base_type->tag.decl_line
+     );
 
  out:
+  return o;
+}
+
+EsObject*
+dwarfish_typedef_convert_to_es     (const struct tag* tag,
+			 struct cu *cu,
+			 DwarfishContext * ctx)
+{
+  struct type* type;
+  EsObject* o;
+  char  buffer[512];
+  
+
+  type = tag__type(tag);
+
+
+  o = es_srealize
+    (
+     "(dwarfish typedef :name %s :size %d :file %, :line %d)",
+     tag__name(&type->namespace.tag, cu, buffer, sizeof(buffer)),
+     tag__size(&type->namespace.tag, cu),
+     O(type->namespace.tag.decl_file? es_string_new(type->namespace.tag.decl_file): es_false) ,
+     type->namespace.tag.decl_line
+     );
+
+  return o;
+}
+
+
+static const struct argp_option dwarfish__options[] = {
+  {
+    .name = NULL,
+  }
+};
+
+static error_t dwarfish__options_parser(int key, 
+					char *arg,
+					struct argp_state *state)
+{
+  switch (key) {
+  case ARGP_KEY_INIT: 
+    state->child_inputs[0] = state->input; break;
+  default:  
+    return ARGP_ERR_UNKNOWN;
+  }
   return 0;
+}
+
+static const char dwarfish__args_doc[] = "DEBUGINFO-FILE";
+
+static struct argp dwarfish__argp = {
+  .options  = dwarfish__options,
+  .parser   = dwarfish__options_parser,
+  .args_doc = dwarfish__args_doc,
+};
+
+struct cus*
+dwarfish_cus_new (char* file)
+{
+  struct cus* cus;
+
+  int err;
+  int argc;
+  char * argv[2];
+
+  argv[0] = "dwarfish";
+  argv[1] = file;
+  argc =  2;
+
+  cus = cus__new(NULL, NULL);
+  if (cus == NULL)
+    {
+      fputs("dwarfish: insufficient memory\n", stderr);
+      return NULL;
+    }
+
+  err = cus__loadfl(cus, &dwarfish__argp, argc, argv);
+  if (err != 0)
+    {
+      fprintf(stderr, "dwarfish: failed to load: %s\n", file);
+      free(cus);
+      return NULL;
+    }
+
+  return cus;
+}
+
+
+static int
+dwarfish_cu_iterator(struct cu *cu, void *cookie)
+{
+  return cu__for_each_tag(cu, 
+			  dwarfish_tag_iterator, 
+			  cookie, 
+			  NULL);
+}
+
+static int
+dwarfish_tag_iterator (struct tag *tag, struct cu *cu, void *cookie)
+{
+  DwarfishContext *ctx;
+
+
+  ctx = cookie;
+  return dwarfish_tag_do(tag, cu, ctx);
 }
--- dive/dwarf/shell/src/dwarfish.h
+++ dive/dwarf/shell/src/dwarfish.h
@@ -12,7 +12,12 @@ struct _DwarfishContext
 };
 
 DwarfishContext * dwarfish_context_new  (void);
-void              dwarfish_context_free (DwarfishContext * fish);
+void              dwarfish_context_do   (DwarfishContext * fish,
+					 struct cus *cus);
+void              dwarfish_context_free (DwarfishContext * ctx);
+
+struct cus*       dwarfish_cus_new      (char* file);
+
 int               dwarfish_tag_do       (struct tag *tag, 
 					 struct cu *cu, 
 					 DwarfishContext * fish);
