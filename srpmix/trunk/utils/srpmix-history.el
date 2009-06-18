1a60d6cd12d1321d4f280aa454ccb95c62959e43
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-06-18 11:51:34 +0000

    Implement M-P, M-N

--- srpmix/trunk/utils/srpmix-history.el
+++ srpmix/trunk/utils/srpmix-history.el
@@ -1,18 +1,61 @@
+(require 'es)
+
 (define-key minibuffer-local-filename-completion-map [(shift meta ?n)] 'srpmix-history-newer-path)
 (define-key minibuffer-local-filename-completion-map [(shift meta ?p)] 'srpmix-history-older-path)
 
+(defun srpmix-history-distro-neighbor (distro prefix newer)
+  (if (string-match "\\(.*\\)\\([0-9]\\)$" distro)
+      (let* ((distro-prefix (match-string 1 distro))
+	     (distro-version (string-to-number (match-string 2 distro)))
+	     (neighbor-distro (format "%s%d"
+				      distro-prefix
+				      (+ distro-version (if newer 1 -1)))))
+	(if (file-directory-p (format "%s/%s" prefix neighbor-distro))
+	    neighbor-distro
+	  nil))
+	nil))
 
+(defconst srpmix-history-compare-script
+  '(begin (use gauche.version)
+	  (write (sort (port->string-list (current-input-port)) 
+		       version<?))))
 
-(defun srpmix-history-distro-neighbor (distro newer)
-  ;; rhel4u3 -> rhel4u4 -> ... nil
-  (if newer
-      "DN"
-    "DO"))
+(defun srpmix-history-index-for (version varray)
+  (let ((l (length varray))
+	(i 0)
+	(found nil))
+    (while (and (< i l) (not found))
+      (if (equal version (aref varray i))
+	  (setq found i)
+	(setq i (+ i 1))))
+    (if found
+	found
+      nil)))
 
-(defun srpmix-history-version-neighbor (distro newer)
-  (if newer
-      "VN"
-    "VO"))
+(defun srpmix-history-version-neighbor (version prefix newer)
+  (when (file-directory-p prefix)
+    (let* ((default-directory prefix)
+	   (script (let ((stream (es-make-output-stream ""))) 
+		     (es-print srpmix-history-compare-script stream) 
+		     (es-stream-get-string stream)))
+	   (result (shell-command-to-string (format "ls %s| gosh -e '%s'"
+						    ;; ???
+						    prefix
+						    script)))
+	   (stream (es-make-input-stream result))
+	   (varray (apply 'vector (es-read stream))))
+      (let ((last-idx (- (length varray) 1))
+	    (idx (srpmix-history-index-for version varray)))
+	(if idx
+	    (if newer
+		(if (< idx last-idx)
+		    (aref varray (+ idx 1))
+		  nil)
+	      (if (< 0 idx)
+		  (aref varray (- idx 1))
+		nil))
+	  nil)))))
+
 
 (defun srpmix-history-neighbor-string (contents
 				       distro-level
@@ -27,7 +70,7 @@
 	  (distro (match-string 2 contents))
 	  (rest   (match-string 3 contents)))
       (when distro-level
-	(let ((neighbor-distro (srpmix-history-distro-neighbor distro newer)))
+	(let ((neighbor-distro (srpmix-history-distro-neighbor distro prefix newer)))
 	  (when neighbor-distro
 	    (concat prefix neighbor-distro rest))))))
      ;; /srv/sources/packages/4/4Suite/rhel4u4/
@@ -39,7 +82,7 @@
 	    (distro (match-string 2 contents))
 	    (rest   (match-string 3 contents)))
 	(when distro-level
-	  (let ((neighbor-distro (srpmix-history-distro-neighbor distro newer)))
+	  (let ((neighbor-distro (srpmix-history-distro-neighbor distro prefix newer)))
 	    (when neighbor-distro
 	      (concat prefix neighbor-distro rest))))))
      ;; /srv/sources/sources/4/4Suite/1.0-3/*     
