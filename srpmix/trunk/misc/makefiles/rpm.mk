ed28767c05f663d0cfbef570aca2956808b3b381
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-07-17 14:26:21 +0000

    * misc/makefiles/rpm.mk: Build srpmix-dir-base.rpm.

--- srpmix/trunk/misc/makefiles/rpm.mk
+++ srpmix/trunk/misc/makefiles/rpm.mk
@@ -3,10 +3,12 @@
 srpm: distcheck
 	$(mkinstalldirs) build/{SPECS,RPMS,BUILD,SRPMS}
 	rpmbuild --define "_topdir `pwd`/build" -ts $(DIST_ARCHIVES)
+	rpmbuild --define "_topdir `pwd`/build" -bs misc/specs/srpmix-dir-base.SPEC
 
 rpm: distcheck
 	$(mkinstalldirs) build/{SPECS,RPMS,BUILD,SRPMS}
 	rpmbuild --define "_topdir `pwd`/build" -ta $(DIST_ARCHIVES)
+	rpmbuild --define "_topdir `pwd`/build" -ba misc/specs/srpmix-dir-base.SPEC
 
 clean-local::
 	/bin/rm -rf build
