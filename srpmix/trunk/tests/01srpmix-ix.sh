95d5541abee2562140008bcb4fc0023fbf29e286
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-07-24 01:08:25 +0000

    * tests/Makefile.am (TESTS_ENVIRONMENT): Set top_srcdir.
    	* tests/01srpmix-ix.sh: Copy macros.srpmix under home directory.
    
    	* tests/01srpmix-ix.sh: Ditto.
    
    	* tests/00srpmix-wrap.sh: Ditto.
    
    	* tests/00srpmix.sh: Ditto.
    
    	* src/srpmix-wrap(RPMBUILD): Don't set.
    
    	* src/libsrpmix.sh.in (RPMBUILD, SWRFBUILD): Use the values in environment
    	variables if available.
    
    	* configure.ac: Improve the way to get absolute path for
    	test srpms.

--- srpmix/trunk/tests/01srpmix-ix.sh
+++ srpmix/trunk/tests/01srpmix-ix.sh
@@ -12,6 +12,14 @@ if test -z "$TEST_SRPMS"; then
     exit 77
 fi
 
+if test -f ~/.rpmmacros; then
+    echo ";;; Use own .rpmmacros exists" 1>&2
+    exit 77
+else
+    cat ${top_srcdir}/src/macros.srpmix >> ~/.rpmmacros
+    trap "rm ~/.rpmmacros" 0
+fi
+
 rm -rf `pwd`/${TESTDIR}
 make DESTDIR=`pwd`/${TESTDIR} -C ${top_builddir} install
 
