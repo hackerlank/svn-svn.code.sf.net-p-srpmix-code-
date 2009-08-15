028c582248ea7c4be196070686e62f5e935bb774
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-08-15 07:27:39 +0000

    updated

--- sbuild/src/sbuild-createrepo.mk
+++ sbuild/src/sbuild-createrepo.mk
@@ -1,8 +1,9 @@
 #!/usr/bin/make -f
 
 #RPMS := $(shell find -name '*.rpm')
-RPMS := $(wildcard [0-9a-zA-Z]) $(wildcard weakview) $(wildcard dir-pkg) $(wildcard *.rpm)
+RPMS := $(wildcard [0-9a-zA-Z]) $(wildcard weakview) $(wildcard dir-pkg/[0-9a-zA-Z]) $(wildcard *.rpm)
 
 all: repodata/primary.xml.gz
-repodata/primary.xml.gz: $(RPMS) comps.xml
-	time createrepo --update -d --skip-stat -g comps.xml .
+repodata/primary.xml.gz: $(RPMS)
+	if test -f comps.xml; then COMPS_OPT="-g comps.xml"; fi \
+        createrepo --update -d --skip-stat $$COMPS_OPT  .
