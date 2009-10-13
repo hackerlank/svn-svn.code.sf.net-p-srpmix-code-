7844e7dcd371b70a93c26f1eefa57b0ed03da0ce
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-10-13 18:20:31 +0000

    Added update.sh.

---
+++ lcopy2/src/conf.d/update.sh
@@ -0,0 +1,14 @@
+dir=$1
+lcopies=${dir}/*.lcopy
+
+cat <<"EOF"
+confddir = $(sysconfdir)/lcopy/conf.d
+dist_confd_DATA = \
+	\
+EOF
+
+for x in $lcopies; do
+    printf "	%s \\\\\n" $(basename $x)
+done
+echo '	\'
+echo '	$(NULL)'
