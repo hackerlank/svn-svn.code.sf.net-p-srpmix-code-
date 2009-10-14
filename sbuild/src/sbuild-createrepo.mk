86bfc92ceb1c786a2dd859e20fe7e2c84b26d2ad
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-10-14 09:58:47 +0000

    * sbuild-createrepo.mk (CREATEREPO_OPT): Don't use --update.

--- sbuild/src/sbuild-createrepo.mk
+++ sbuild/src/sbuild-createrepo.mk
@@ -2,8 +2,10 @@
 
 #RPMS := $(shell find -name '*.rpm')
 RPMS := $(wildcard [0-9a-zA-Z]) $(wildcard weakview) $(wildcard dir-pkg/[0-9a-zA-Z]) $(wildcard *.rpm)
+# CREATEREPO_OPT = --update
+CREATEREPO_OPT =
 
 all: repodata/primary.xml.gz
 repodata/primary.xml.gz: $(RPMS)
 	if test -f comps.xml; then COMPS_OPT="-g comps.xml"; fi; \
-		createrepo --update -d --skip-stat $$COMPS_OPT  .
+		createrepo $(CREATEREPO_OPT) -d --skip-stat $$COMPS_OPT  .
