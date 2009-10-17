b3d213624e4a1a6f2f235a4578f0608387d1a941
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-10-17 20:05:15 +0000

    * configure.ac: version 0.1.4.
    	* src/sbuild (run_creterepo): Pass $CREATEREPO_OPTS
    	to make.
    	(run_rsync): Copy sbuild direntry instead of blacklist dir.
    
    	* src/sbuild-createrepo.mk (CREATEREPO_OPTS): Renamed
    	from CREATEREPO_OPT.

--- sbuild/src/sbuild-createrepo.mk
+++ sbuild/src/sbuild-createrepo.mk
@@ -2,10 +2,9 @@
 
 #RPMS := $(shell find -name '*.rpm')
 RPMS := $(wildcard [0-9a-zA-Z]) $(wildcard weakview) $(wildcard dir-pkg/[0-9a-zA-Z]) $(wildcard *.rpm)
-# CREATEREPO_OPT = --update
-CREATEREPO_OPT =
+CREATEREPO_OPTS =
 
 all: repodata/primary.xml.gz
 repodata/primary.xml.gz: $(RPMS)
 	if test -f comps.xml; then COMPS_OPT="-g comps.xml"; fi; \
-		createrepo $(CREATEREPO_OPT) -d --skip-stat $$COMPS_OPT  .
+		createrepo $(CREATEREPO_OPTS) -d --skip-stat $$COMPS_OPT  .
