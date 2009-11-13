5710f99603510835dd8b54f03f73af7c2682d847
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-11-13 21:06:22 +0000

    updated

--- sstat/sstat-pre.scm
+++ sstat/sstat-pre.scm
@@ -2,12 +2,28 @@
 #| -*- scheme -*- |#
 :; exec gosh -- $0 "$@"
 
+(use gauche.net)
+
+(define (make-readable ip)
+  ;; (ip    (inet-address->string (string->number (match 1)) AF_INET))
+  ip)
+
+(define (complete partial)
+  partial)
+
 (define (main args)
   (let loop ((line (read-line)))
     (unless (eof-object? line)
-      (let1 match (#/([0-9]+) (.*)$/ line)
+      (let1 match (#/([0-9]+) (.*)\/$/ line)
 	(if match
-	    )
-      (loop (read-line))
-      )))
-;; IP FILE
+	    (let (
+		  (ip    (make-readable (match 1)))
+		  (time  (sys-time))
+		  (file  (complete (match 2))))
+	      ;;
+	      (write `(nfsd-open-pre :ip ,ip :time ,time :path ,file))
+	      (newline)
+	      )
+	    (format (current-error-port) ";; Wrong input: ~s\n" line)
+	    ))
+      (loop (read-line)))))
