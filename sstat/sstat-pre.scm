64f504a757ab0502c34891b755e08a1a3a60c315
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-11-14 20:36:43 +0000

    updated

--- sstat/sstat-pre.scm
+++ sstat/sstat-pre.scm
@@ -12,18 +12,38 @@
   partial)
 
 (define (main args)
-  (let loop ((line (read-line)))
+  (let loop ((line (read-line))
+	     (port #f)
+	     (tmf  #f))
     (unless (eof-object? line)
-      (let1 match (#/([0-9]+) (.*)\/$/ line)
-	(if match
-	    (let (
-		  (ip    (make-readable (match 1)))
-		  (time  (sys-time))
-		  (file  (complete (match 2))))
-	      ;;
-	      (write `(nfsd-open-pre :ip ,ip :time ,time :path ,file))
-	      (newline)
-	      )
-	    (format (current-error-port) ";; Wrong input: ~s\n" line)
-	    ))
-      (loop (read-line)))))
+      (rxmatch-if (#/^([0-9]+) (.*)\/$/ line)
+	  (#f ip file)
+	(let1 time (sys-time)
+	  (receive (port tmf) (log-port time port tmf)
+	    (write `(nfsd-open-pre :ip ,(string->number ip) :time ,time :path ,file)
+		   port)
+	    (newline port)
+	    (loop (read-line) port tmf)))
+	(begin 
+	  (format (current-error-port) ";; Wrong input: ~s\n" line)
+	  (loop (read-line) port tmf))))))
+
+(define (format-port-name tmf)
+  (format "/tmp/sstat-~a.es" tmf))
+
+(define (log-port time port tmf)
+  (let1 tm0 (sys-localtime time)
+    (let1 tmf0 (format "~d~d~d" 
+		       (+ (ref tm0 'year) 1900)
+		       (+ (ref tm0 'mon) 1)
+		       (ref tm0 'mday))
+      (cond
+       ((equal? tmf tmf0)
+	(values port tmf))
+       ((not tmf)
+	(values (open-output-file (format-port-name tmf0))
+		tmf0))
+       (else
+	(close-output-port port)
+	(values (open-output-file (format-port-name tmf0))
+		tmf0))))))
