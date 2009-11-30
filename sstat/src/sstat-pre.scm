a2682d3807724a646f30867f82d1730abb0c1dfd
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-11-30 10:46:00 +0000

    Updated.

--- sstat/src/sstat-pre.scm
+++ sstat/src/sstat-pre.scm
@@ -2,59 +2,95 @@
 #| -*- scheme -*- |#
 :; exec gosh -- $0 "$@"
 (use gauche.net)
-(us gauche.parseopt)
+(use gauche.parseopt)
+(use srfi-1)
 
 (define (complete partial)
   partial)
 
 
+(define delta 1)
 (define (build-acceptor unacceptable-ip-list0 acceptable-path-regex0)
   (let (
-	(unacceptable-ip-list    (map inet-string->address unacceptable-ip-list0))
+	(unacceptable-ip-list (delete-duplicates 
+			       (apply 'append
+				      (map (cute ref <> 'addresses)
+					   (map sys-gethostbyname
+						(map inet-string->address 
+						     unacceptable-ip-list0))))))
 	(acceptable-path-regex   (string->regexp acceptable-path-regex0))
+	(frequency-table         (make-hash-table 'eq?))
 	)
-    (lambda (ip file)
+    (lambda (ip file time)
       (if (memq ip unacceptable-ip-list)
 	  #f
 	  (if (acceptable-path-regex file)
-	      #t
+	      ;; 
+	      (let1 last-access-time (hash-table-get frequency-table ip #f)
+		(if last-access-time
+		    (if (< delta (- time last-access-time))
+			(begin
+			  (hash-table-put! frequency-table ip time)
+			  #t)
+			#f)
+		    (begin
+		      (hash-table-put! frequency-table ip time)
+		      #t)))
 	      #f)))))
 
+
+(define (print-usage prog-name port exit-status)
+  (format port "Usage: \n")
+  (format port "~a -h|--help\n" prog-name)
+  (format port "~a --sstat-dir=DIR --acceptable-regex=REGEX [IP-ADDRESS]...\n" prog-name)
+  (sys-exit exit-status))
+
 (define (main args)
-  (let ((accept? (build-acceptor '("192.168.11.41" "127.0.0.1")
-				 "^var/lib/srpmix/sources/[0-9a-zA-Z]/[^/]+/[^/]+/.+"))
-	(sstat-dir "/srv/sources/attic/sstat"))
+  (let-args (cdr args)
+      ((help      "h|help" => (cut print-usage (car args) (current-output-port) 0))
+       (sstat-dir "sstat-dir=s" #f)
+       (acceptable-regex "acceptable-regex=s" #f)
+       . ips)
+    (if sstat-dir
+	(when file-is-directory? sstat-dir
+	      (errorf "No such directory: ~a" sstat-dir))
+	(print-usage (car args) (current-error-port) 1))
+    (unless acceptable-regex
+      (print-usage (car args) (current-error-port) 1))
 
-    (when file-is-directory? sstat-dir
-	  (errorf "No such directory: ~a" sstat-dir))
+    (let1 ips (if (member "127.0.0.1" ips)
+		  ips
+		  (reverse (cons "127.0.0.1" (reverse ips))))
+      (let1 accept? (build-acceptor ips)
+	(run accept? sstat-dir)))))
 
-    (run accept? sstat-dir)))
 
 (define (format-port-name sstat-dir tmf)
   (format "~a/sstat-~a.es" sstat-dir tmf))
+
 
 (define (run accept? sstat-dir)
   (let loop ((line (read-line))
 	     (port #f)
 	     (tmf  #f)
 	     (ip    "0.0.0.0")
-	     (time  0))
+	     )
     (unless (eof-object? line)
       (rxmatch-if (#/^([0-9]+) (.*)\/$/ line)
 	  (#f ip file)
 	;; 
 	(let1 ip (string->number ip)
-	  (when (accept? ip file)
-	    (let1 time (sys-time)
+	  (let1 time (sys-time)
+	    (when (accept? ip file time)
 	      (receive (port tmf) (log-port time port tmf sstat-dir)
 		(write `(nfsd-open-pre :ip ,ip :time ,time :path ,file)
 		       port)
 		(newline port)
-		(loop (read-line) port tmf ip time))))
-	  (loop (read-line) port tmf ip time))
+		(loop (read-line) port tmf ip))))
+	  (loop (read-line) port tmf ip))
 	(begin 
 	  (format (current-error-port) ";; Wrong input: ~s\n" line)
-	  (loop (read-line) port tmf ip time))))))
+	  (loop (read-line) port tmf ip))))))
 
 (define (log-port time port tmf sstat-dir)
   (let1 tm0 (sys-localtime time)
