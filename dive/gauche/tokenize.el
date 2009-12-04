0433646f789b633b162fe99adde68b06e4471b3d
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-12-04 22:13:04 +0000

    *** empty log message ***

--- dive/gauche/tokenize.el
+++ dive/gauche/tokenize.el
@@ -1,59 +1,93 @@
-(defun can-go-down   (s)
+(defun tx-can-go-down   (s)
   (memq (string-to-char s) '(?\( ?\{ ?\[))  
   )
 
-(defun calibrate-pos ()
+(defun tx-calibrate-pos ()
   (forward-sexp 1)
   (backward-sexp 1))
 
-(defun token (str b e)
+(defun tx-token (str b e)
   (vector str b e))
 
-(defun get-expressions00 (bias)
-  (calibrate-pos)
+(defun tx-get-expressions00 (bias)
+  (tx-calibrate-pos)
   (let ((p (point)))
     (forward-sexp 1)
     (let ((s (buffer-substring-no-properties p (point))))
       (cond
        ((eobp)
-	(list (token s
-		     (+ p bias)
-		     (+ (point) bias))))
-       ((can-go-down s)
-;;	(get-expressions0 (substring s 1 -1) (+ (+ (- p 1) 1) bias) t)
-	(append
-	 (list (token (substring s 0 1)
-		      (+ p bias)
-		      (+ (+ p bias) 1)))
-	 (get-expressions0 s (+ (- p 1) bias) t)
-	 (list (token (substring s (1- (length s))) 
-		      (+ (+ p bias) (1- (length s)))
-		      (+ (+ p bias) (1- (length s)) 1)
-		      )))
+	(list (tx-token s
+			(+ p bias)
+			(+ (point) bias))))
+       ((tx-can-go-down s)
+	;;	(tx-get-expressions0 (substring s 1 -1) (+ (+ (- p 1) 1) bias) t)
+	(let ((p+ (+ p bias))
+	      (l-  (1- (length s))))
+	  (append
+	   (list (tx-token (substring s 0 1) p+ (+ p+ 1)))
+	   (tx-get-expressions1 s (- p+ 1) t)
+	   (list (tx-token (substring s l-) 
+			   (+ p+ l-)
+			   (+ p+ l- 1)
+			   ))))
 	;;
 	)
        (t
-	(get-expressions0 s (+ (- p 1) bias))
+	(tx-get-expressions1 s (+ (- p 1) bias))
 	)))))
 
-(defun get-expressions0 (in &optional bias downlist)
-  (with-temp-buffer
-    (insert in)
-    (goto-char (point-min))
-    (when downlist
-      (down-list 1))
-    (let ((result (list)))
-      (condition-case err
-	  (while (not (eobp))
-	    (let ((r0 (get-expressions00 (or bias 0))))
-	      (if (equal (car (reverse r0)) (car (reverse result)))
-		  (goto-char (point-max))
-		(setq result (append result r0))
-		)))
-	(error result))
-      result
-      )))
+(defun tx-get-expressions1 (in &optional bias downlist)
+  (let ((s0 (buffer-string))
+	(p0 (point)))
+    (erase-buffer)
+    (let ((r (tx-get-expressions01 in bias downlist)))
+      (erase-buffer)
+      (insert s0)
+      (goto-char p0)
+      r)))
 
-(defun get-expressions (in &optional bias)
-  (get-expressionos0 in bias))
-	
+(defun tx-get-expressions01 (in &optional bias downlist)
+  (insert in)
+  (goto-char (point-min))
+  (when downlist
+    (down-list 1))
+  (let ((result (list)))
+    (condition-case err
+	(while (not (eobp))
+	  (let ((r0 (tx-get-expressions00 (or bias 0))))
+	    (if (equal (car (last r0)) (car (last result)))
+		(goto-char (point-max))
+	      (setq result (append result r0))
+	      )))
+      (error result))
+    result
+    ))
+
+(defun tx-get-expressions0 (in &optional bias downlist)
+  (with-temp-buffer
+    (c-mode)
+    (tx-get-expressions01 in bias downlist)
+    ))
+
+(defun tx-get-expressions (in &optional bias)
+  (tx-get-expressions0 in bias))
+
+(require 'which-func)
+(defun tx-get-expressions-here ()
+  (let ((func (condition-case nil
+		  (which-function)
+		(error nil)))	)
+    (when func
+      (let ((range (save-excursion (beginning-of-defun)
+				   (let ((b (point)))
+				     (let ((e (when (re-search-forward "{" nil t)
+						(end-of-defun)
+						(point))))
+				       (if e
+					   (list b e)
+					 nil))))))
+	(if range
+	    (if (and (<= (car range) (point))
+		     (<= (point) (cadr range)))
+		(tx-get-expressions (buffer-substring-no-properties (car range) (cadr range)) (car range))
+	      nil))))))
