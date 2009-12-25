32925e079f3dd6fca5bacf7f50038a592afffb8e
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2009-12-25 09:25:16 +0000

    *** empty log message ***

--- logma/logma.scm
+++ logma/logma.scm
@@ -6,17 +6,47 @@
 	  (else ""))
 	 (read-line)))
 
+(define (print-help prog status)
+  (display "Usage: \n")
+  (format #t "	~s DELTA\n" prog)
+  (format #t "	~s --help\n" prog)
+  (exit status))
+
+(define (rearrange date-string delta)
+  (date->string 
+   (time-utc->date
+    (add-duration (date->time-utc
+		   (string->date (string-append 
+				  (date->string (time-utc->date (current-time)) "~y")
+				  " " date-string) "~y ~b ~d ~H:~M:~S"))
+		  delta))
+   "~b ~d ~H:~M:~S"))
+
 (define (main args)
-  (let loop ((l (read-line-safe)))
-    (unless (eof-object? l)
-      ;; Dec 14 16:56:29 host xinetd[2559]: START: ndtp pid=16222 from=::ffff:127.0.0.1
-;      (print l)
-      (rxmatch-cond
-	((#/^([A-Z][a-z][a-z] [0-9]+ [0-9]{2}:[0-9]{2}:[0-9]{2}) ([-a-z0-9]+) ([^\[\]\/]+)(\[[0-9]+\])?: (.*)$/ l)
-	 (#f date host cmd pid msg)
-	 (print (string->date (string-append 
-			       (date->string (time-utc->date (current-time)) "~y")
-			       " "date) "~y ~b ~d ~H:~M:~S"))
-	 ))
-      (loop (read-line-safe)))))
+  (unless (eq? 2 (length args))
+    (with-output-to-port (current-error-port)
+      (cute print-help (car args) 1)))
+  (when (or (equal? (cadr args) "-h")
+	    (equal? (cadr args) "--help"))
+    (print-help (car args) 0))
+
+  (let1 delta (let1 d (string->number (cadr args))
+		(unless d
+		  (with-output-to-port (current-error-port)
+		    (cute print-help (car args) 1)))
+		(make <time> :type 'time-duration :second d))
+    (let loop ((l (read-line-safe)))
+      (unless (eof-object? l)
+	(rxmatch-cond
+	  ((#/^([A-Z][a-z][a-z] [0-9]+ [0-9]{2}:[0-9]{2}:[0-9]{2}) ([-a-z0-9]+) ([^\[\]\/]+)(\[[0-9]+\])?: (.*)$/ l)
+	   (#f date host cmd pid msg)
+	   (format #t
+		   "~a ~a ~a ~a: ~a\n"
+		   (rearrange date delta)
+		   host
+		   cmd
+		   (or pid "")
+		   msg
+		   )))
+	(loop (read-line-safe))))))
 
