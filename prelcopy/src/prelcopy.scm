9557ebafcdec6bde828a90eccf5321459a56dea2
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-01-04 20:18:20 +0000

    *** empty log message ***

---
+++ prelcopy/src/prelcopy.scm
@@ -0,0 +1,5 @@
+#;(prelcopy :package "JBossEAP"
+          :branch  "trunk"
+          :command-line "svn checkout http://anonsvn.jboss.org/repos/jbossas/trunk"
+	  :update #t
+          :generated-by "svnweb+jbossas.prelcopy")
