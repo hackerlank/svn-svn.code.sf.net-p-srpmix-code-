b568d57165bce5b074fe1483a69b8f112d2d21c3
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-01-05 12:04:25 +0000

    *** empty log message ***

---
+++ srpmix/trunk/utils/rpm-spec-eldoc.el
@@ -0,0 +1,28 @@
+;; /usr/bin/bugzilla is expected to be installed.
+(require 'thingatpt)
+
+(add-hook 'rpm-spec-mode-hook 'rpm-spec-eldoc-install)
+
+(defvar rpm-spec-eldoc-hash (make-hash-table :test 'eq))
+(defun rpm-spec-eldoc-install ()
+  (set (make-local-variable 'eldoc-documentation-function)
+       #'rpm-spec-eldoc-function))
+
+(defun rpm-spec-eldoc-function ()
+  (when (save-excursion (re-search-backward "^%changelog" nil t))
+    (let ((num (thing-at-point 'symbol)))
+      (cond
+       ((string-match "[0-9]\\{5,\\}" num)
+	(setq num (string-to-number num))
+	(or (gethash num rpm-spec-eldoc-hash)
+	    (let ((r (shell-command-to-string (format "/usr/bin/bugzilla query -b %d --outputformat='%%{bug_id}: %%{short_desc}'"
+					 num))))
+	      (if (equal r "")
+		  (setq r "bz: cannot find entry")
+		(setq r (substring r 0 (1- (length r)))))
+	      (puthash num r rpm-spec-eldoc-hash)
+	      r)))
+       (t
+	nil)))))
+
+(provide 'rpm-spec-eldoc)
