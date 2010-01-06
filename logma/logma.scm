a7ec890af42db025ecef24d82b615c6c139ff6e1
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-01-06 10:12:47 +0000

    *** empty log message ***

--- logma/logma.scm
+++ logma/logma.scm
@@ -24,7 +24,7 @@
 (define (read-line-safe . args)
   (guard (e
 	  (else ""))
-	 (read-line   
+	 (read-line/nl   
 	  (if (null? args) (current-input-port) (car args)))))
 
 (define (print-help prog status)
@@ -113,41 +113,87 @@
 				    (time-utc->date (car elt)))
 				   (cdr elt))))
      (stable-sort!
-      (map (lambda (l)
-	     (rxmatch-cond
-	       ((var-log-message-line-regex l)
-		(#f date host cmd pid msg)
-		(list (date->time-utc (string->date+year$ date))
-		      host 
-		      cmd
-		      pid
-		      msg))
-	       ((var-log-message-repeated-line-regex l)
-		(#f date host msg)
-		(list (date->time-utc (string->date+year$ date))
-		      host 
-		      #f
-		      #f
-		      msg))
-	       ((var-log-message-signal-line-regex l)
-		(#f date host msg)
-		(list (date->time-utc (string->date+year$ date))
-		      host 
-		      #f
-		      #f
-		      msg))
-	       (else
-		#?=l)
-	       ))
-	   (delete! "" 
-		    (port->list read-line-safe (current-input-port))
-		    equal?))
+      (delete #f 
+	      (map (lambda (l)
+		     (rxmatch-cond
+		       ((var-log-message-line-regex l)
+			(#f date host cmd pid msg)
+			(list (date->time-utc (string->date+year$ date))
+			      host 
+			      cmd
+			      pid
+			      msg))
+		       ((var-log-message-repeated-line-regex l)
+			(#f date host msg)
+			(list (date->time-utc (string->date+year$ date))
+			      host 
+			      #f
+			      #f
+			      msg))
+		       ((var-log-message-signal-line-regex l)
+			(#f date host msg)
+			(list (date->time-utc (string->date+year$ date))
+			      host 
+			      #f
+			      #f
+			      msg))
+		       (else
+			;; TODO
+			#?=l
+			#f)
+		       ))
+		   (delete! "" 
+			    (port->list read-line-safe (current-input-port))
+			    equal?)))
       (lambda (a b)
 	(if (time=? (car a) (car b))
 	    (if (string=? (cadr a) (cadr b))
 		#f
 		(string<? (cadr a) (cadr b)))
 	    (time<? (car a) (car b))))))))
+
+
+;;;
+;;; mime.scm - parsing MIME (rfc2045) message
+;;;  
+;;;   Copyright (c) 2000-2009  Shiro Kawai  <shiro@acm.org>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+;;;  $Id: mime.scm,v 1.17 2008-05-10 13:36:08 shirok Exp $
+;;;
+(define (read-line/nl inp)
+    (let loop ((c (read-char inp))
+               (chars '()))
+      (cond [(eof-object? c)
+             (if (null? chars) c  (list->string (reverse! chars)))]
+            [(char=? c #\newline) (list->string (reverse! chars))]
+            [else (loop (read-char inp) (cons c chars))])))
 
 (define (main args)
   (unless (<= 2 (length args))
