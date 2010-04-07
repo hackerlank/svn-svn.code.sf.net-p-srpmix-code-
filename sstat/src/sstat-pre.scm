c2cddcea444710dfdb361e295854059f41d01412
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-04-07 12:08:53 +0000

    * configure.ac: version 0.3.1.
    	* src/sstat-pre.scm (read-line-safe): New function.
    	(run): Use `read-line-safe'.

--- sstat/src/sstat-pre.scm
+++ sstat/src/sstat-pre.scm
@@ -20,7 +20,7 @@
 						     unacceptable-ip-list0))))
 			       equal?))
 	(acceptable-path-regex   (string->regexp acceptable-path-regex0))
-	(multiaccess-table        (make-hash-table 'eq?))
+	(multiaccess-table       (make-hash-table 'eq?))
 	)
     (lambda (ip file time)
       (if (memq ip unacceptable-ip-list)
@@ -64,13 +64,23 @@
     (format "~a/sstat-~a.es" dir tmf)))
 
 
+(define (read-line-safe . port)
+  (let1 port (if (null? port)
+		 (current-input-port)
+		 (car port))
+    (guard (e
+	    (else #f))
+	   (read-line port))))
+
 (define (run accept? sstat-dir)
-  (let loop ((line (read-line))
+  (let loop ((line (read-line-safe))
 	     (port #f)
 	     (tmf  #f)
 	     (ip    "0.0.0.0")
 	     )
-    (unless (eof-object? line)
+    (cond
+     ((not line) (loop (read-line-safe) port tmp ip))
+     ((not (eof-object? line))
       (rxmatch-if (#/^([0-9]+) ([0-9]+) (.*)\/$/ line)
 	  (#f ip time file)
 	;; 
@@ -81,11 +91,11 @@
 		(write `(nfsd-open-pre :ip ,ip :time ,time :path ,file)
 		       port)
 		(newline port)
-		(loop (read-line) port tmf ip)))
-	  (loop (read-line) port tmf ip))
+		(loop (read-line-safe) port tmf ip)))
+	  (loop (read-line-safe) port tmf ip))
 	(begin 
 	  (format (current-error-port) ";; Wrong input: ~s\n" line)
-	  (loop (read-line) port tmf ip))))))
+	  (loop (read-line-safe) port tmf ip)))))))
 
 (define (log-port time port tmf sstat-dir)
   (let1 tm0 (sys-localtime time)
