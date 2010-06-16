45485243a4ad14838729fe228762c47f0815e3c4
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-06-16 17:10:51 +0000

    *** empty log message ***

--- yogomacs/src/gauche/yogomacs/dired.scm
+++ yogomacs/src/gauche/yogomacs/dired.scm
@@ -13,63 +13,92 @@
 ;;                    <========>
 ;;                             <============>
 ;;                             <=========================> htmlprag
+
+;; . -> /srv/sources...
+;; .. -> /srv/sources...
+;; http://planet.plt-scheme.org/package-source/lizorkin/ssax.plt/2/0/SXML-tree-trans.ss
 (define-module yogomacs.dired
-  (export dired)
+  (export dired
+	  dired-faces)
+  (use file.util)
+  (use util.combinations)
   )
 (select-module yogomacs.dired)
 
-(define (dired dir)
-  `(*TOP* (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"")
-	  "\n"
-	  (*DECL* DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd")
-	  "\n"
-	  (html (|@| 
-		 (xmlns "http://www.w3.org/1999/xhtml")
-		 (xml:lang "en")
-		 (lang "en")
-		 )
-		(head 
-		 (title "/sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2")
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/default.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/highlight.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/linum.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/fringe.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/dired-header.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/dired-directory.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/dired-marked.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/dired-symlink.css"))) "\n"
-		 (link (|@| (rel "stylesheet") (type "text/css") (href "http://srpmix.org/api/css/dired-perm-write.css"))) "\n"
-		 ) 
-		(body 
-		 (script (|@| (src "http://srpmix.org/api/js/biwascheme.js")) "\n" 
-			 "(load \"http://srpmix.org/api/scm/yogomacs.scm\")\n") "\n" 
-			 (pre 
-			  (span (|@| (class "linum") (id "linum:1")) "1")
-			  (span (|@| (class "fringe") (id "name:.srpmix_dired_header")) " ")
-			  (span (|@| (class "dired-header")) "  /sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2") ":\n"
-			  (span (|@| (class "linum") (id "linum:2")) "2")
-			  (span (|@| (class "fringe") (id "name:.srpmix_dired_total_line")) " ")
-			  "  total used in directory 33 available 1152921504606846976\n"
-			  (span (|@| (class "linum") (id "linum:3")) "3")
-			  (span (|@| (class "fringe") (id "name:.")) " ")
-			  "  drms n sources srpmix 4096  1983-09-27 12:35 "
-			  (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2&amp;display=font-lock")) (span (|@| (class "dired-directory")) "."))
-			  "\n"
-			  (span (|@| (class "linum") (id "linum:4")) "4")
-			  (span (|@| (class "fringe") (id "name:..")) " ")
-			  "  drms n sources srpmix 4096  1983-09-27 12:35 "
-			  (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build&amp;display=font-lock")) (span (|@| (class "dired-directory")) ".."))
-			  "\n"
-			  (span (|@| (class "linum") (id "linum:5")) "5")
-			  (span (|@| (class "fringe") (id "name:ChangeLog")) " ")
-			  "  -rms n sources srpmix 4096  1983-09-27 12:35 "
-			  (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/ChangeLog&amp;display=font-lock")) "ChangeLog")
-			  "\n"
-			  (span (|@| (class "linum") (id "linum:6")) "6")
-			  (span (|@| (class "fringe") (id "name:lisp")) " ")
-			  "  drms n sources srpmix 4096  1983-09-27 12:35 "
-			  (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/lisp&amp;display=font-lock")) (span (|@| (class "dired-directory")) "lisp"))
-			  "\n") (div (|@| (class "default") (id "bs-console"))) "\n")))
+(define template `(*TOP* 
+		   (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"") "\n"		 
+		   (*DECL* DOCTYPE html PUBLIC 
+			   "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd")
+		   (html (|@| 
+			  (xmlns "http://www.w3.org/1999/xhtml")
+			  (xml:lang "en")
+			  (lang "en")
+			  )
+			 (head 
+			  (title)
+			  (stylesheets)
+			  )
+			 (body))))
+(define dired-faces '(
+		      default
+		      highlight
+		      linum
+		      lfringe
+		      rfringe
+		      dired-header
+		      dired-directory
+		      dired-symlink
+		      ))
+(define dired-styles '(Default Invert))
+
+(define (face->ccs-url face style ccs-prefix)
+  (format "~a/~a--~a.css" ccs-prefix face style))
+
+(define (stylesheets ccs-prefix)
+  (map
+   (lambda (face-style)
+     `(link (|@| 
+	     (rel "stylesheet")
+	     (type "text/css")
+	     (href ,(face-ccs-url (car face-style) (cadr face-style) ccs-prefix))))
+     )
+   (cartesian-product `(,dired-faces
+		       ,dired-styles ))))
+
+(define (entry name line)
+  `(
+    (span (|@| (class "linum") (id ,(format "L:~d" line)))
+	  (a (href "#N:~a" name) (format "~d" line))
+	  )
+    
+    
+  
+  
+(define (body dir)
+  (pre 
+   (span (|@| (class "linum") (id "linum:3")) "3")
+   (span (|@| (class "fringe") (id "name:.")) " ")
+   "  drms n sources srpmix 4096  1983-09-27 12:35 "
+   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2&amp;display=font-lock")) (span (|@| (class "dired-directory")) "."))
+   "\n"
+   (span (|@| (class "linum") (id "linum:4")) "4")
+   (span (|@| (class "fringe") (id "name:..")) " ")
+   "  drms n sources srpmix 4096  1983-09-27 12:35 "
+   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build&amp;display=font-lock")) (span (|@| (class "dired-directory")) ".."))
+   "\n"
+   (span (|@| (class "linum") (id "linum:5")) "5")
+   (span (|@| (class "fringe") (id "name:ChangeLog")) " ")
+   "  -rms n sources srpmix 4096  1983-09-27 12:35 "
+   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/ChangeLog&amp;display=font-lock")) "ChangeLog")
+   "\n"
+   (span (|@| (class "linum") (id "linum:6")) "6")
+   (span (|@| (class "fringe") (id "name:lisp")) " ")
+   "  drms n sources srpmix 4096  1983-09-27 12:35 "
+   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/lisp&amp;display=font-lock")) (span (|@| (class "dired-directory")) "lisp"))
+   "\n")
+  )
+
+(define (dired dir ccs-prefix)
   )
 
 (provide "yogomacs/dired")
