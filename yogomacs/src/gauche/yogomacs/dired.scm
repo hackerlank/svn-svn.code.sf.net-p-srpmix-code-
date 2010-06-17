07962c7d3000a5701f171bdca25a12ea650fa87b
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-06-17 18:44:37 +0000

    *** empty log message ***

--- yogomacs/src/gauche/yogomacs/dired.scm
+++ yogomacs/src/gauche/yogomacs/dired.scm
@@ -22,32 +22,22 @@
 	  dired-faces)
   (use file.util)
   (use util.combinations)
+  (use gauche.sequence)
   )
 (select-module yogomacs.dired)
 
-(define template `(*TOP* 
-		   (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"") "\n"		 
-		   (*DECL* DOCTYPE html PUBLIC 
-			   "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd")
-		   (html (|@| 
-			  (xmlns "http://www.w3.org/1999/xhtml")
-			  (xml:lang "en")
-			  (lang "en")
-			  )
-			 (head 
-			  (title)
-			  (stylesheets)
-			  )
-			 (body))))
 (define dired-faces '(
 		      default
 		      highlight
 		      linum
 		      lfringe
 		      rfringe
+		      dired-regular	;TODO
 		      dired-header
 		      dired-directory
 		      dired-symlink
+		      dired-symlink-arrow ;TODO
+		      dired-symlink-to	  ;TODO
 		      ))
 (define dired-styles '(Default Invert))
 
@@ -65,40 +55,95 @@
    (cartesian-product `(,dired-faces
 		       ,dired-styles ))))
 
-(define (entry name line)
-  `(
-    (span (|@| (class "linum") (id ,(format "L:~d" line)))
-	  (a (href "#N:~a" name) (format "~d" line))
-	  )
-    
-    
-  
-  
-(define (body dir)
-  (pre 
-   (span (|@| (class "linum") (id "linum:3")) "3")
-   (span (|@| (class "fringe") (id "name:.")) " ")
-   "  drms n sources srpmix 4096  1983-09-27 12:35 "
-   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2&amp;display=font-lock")) (span (|@| (class "dired-directory")) "."))
-   "\n"
-   (span (|@| (class "linum") (id "linum:4")) "4")
-   (span (|@| (class "fringe") (id "name:..")) " ")
-   "  drms n sources srpmix 4096  1983-09-27 12:35 "
-   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build&amp;display=font-lock")) (span (|@| (class "dired-directory")) ".."))
-   "\n"
-   (span (|@| (class "linum") (id "linum:5")) "5")
-   (span (|@| (class "fringe") (id "name:ChangeLog")) " ")
-   "  -rms n sources srpmix 4096  1983-09-27 12:35 "
-   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/ChangeLog&amp;display=font-lock")) "ChangeLog")
-   "\n"
-   (span (|@| (class "linum") (id "linum:6")) "6")
-   (span (|@| (class "fringe") (id "name:lisp")) " ")
-   "  drms n sources srpmix 4096  1983-09-27 12:35 "
-   (a (|@| (href "http://srpmix.org/api/browse.cgi?path=sources/e/emacs/23.2-3.fc14/pre-build/emacs-23.2/lisp&amp;display=font-lock")) (span (|@| (class "dired-directory")) "lisp"))
-   "\n")
-  )
+(define (line dir entry linum linum-column make-url result)
+  (append (revese (line0 dir entry linum linum-column make-url)) result))
 
-(define (dired dir ccs-prefix)
-  )
+(define (line0 dir entry linum linum-column make-url)
+  (let1 path (build-path dir entry)
+    `(
+      (span (|@| (class "linum") (id ,(format "N:~a/L:~a" entry linum))) 
+	    ,(format (string-append "~" (nubmer->string linum-column) ",d")
+		     (nubmer->string linum)))
+      (span (|@| (class "lfringe") (id ,(format "f:L/N:~a/L:~d" entry linum))) " ")
+      (span (|@| (class "rfringe") (id "f:R/L:~d") linum) " ")
+      ,@(cond
+	 ((equal? entry ".")
+	  `(
+	    ,(format "  ~a ~d ~a " #\d 4096 "Apr 27 00:03 ")
+	    (span (|@| (class "dired-directory")) 
+		  (a (|@| (href ,(make-url dir entry 'current-directory)))
+		     ,entry))
+	    ))
+	 ((equal? entry "..")
+	  `(
+	    ,(format "  ~a ~d ~a " #\d 4096 "Apr 27 00:03")
+	    (span (|@| (class "dired-directory")) 
+		  (a (|@| (href ,(make-url path dir entry 'parent-directory)))
+		     ,entry))
+	   ))
+	 ((file-is-symlink? path)
+	  (let1 to "/dev/null"		;TODO
+	    `(
+	      ,(format "  ~a ~d ~a " #\l 4096 "Apr 27 00:03")
+	      (span (|@| (class "dired-symlink"))
+		    (a (|@| (href ,(make-url path dir entry 'symlink to)))
+		       ,entry))
+	      (span (|@| (class "dired-symlink-arrow"))
+		   " -> ")
+	      (span (|@| (class "dired-symlink-to"))
+		    ;; TODO
+		    "/dev/null")
+	     )))
+	 ((file-is-directory? path)
+	  `(
+	    ,(format "  ~a ~d ~a " #\d 4096 "Apr 27 00:03")
+	    (span (|@| (class "dired-directory")) 
+		  (a (|@| (href ,(make-url path dir entry 'directory)))
+		     ,entry))))
+	 ((file-is-regular? path)
+	  `(
+	    ,(format "  ~a ~d ~a " #\d 4096 "Apr 27 00:03")
+	    (span (|@| (class "dired-regular")) 
+		  (a (|@| (href ,(make-url path dir entry 'regular)))
+		     ,entry))))
+	 (else
+	  (list))))))
+
+(define (dired path filter ccs-prefix)
+  (receive (dir entries)
+      (if file-is-directory? path
+	  (values path (directory-list path
+				       :add-path? #f 
+				       :children? #t
+				       :filter (if filter
+						   filter
+						   (lambda (e) #t))))
+	  (values (sys-dirname path) (let1 basename (sys-basename path)
+				       (if filter
+					   (if (filter basename)
+					       (list basename)
+					       (list))
+					   (list basename)))))
+    (cute dired0 <> <> css-prefix)))
+
+(define (dired0 dir entries make-url css-prefix)
+  `(*TOP* 
+    (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"") "\n"
+    (*DECL* DOCTYPE html PUBLIC 
+	    "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd") "\n"
+	    (html (|@| (xmlns "http://www.w3.org/1999/xhtml") (xml:lang "en") (lang "en"))
+		  (head 
+		   (title ,dir)
+		   ,@(stylesheets css-prefix))
+		  (body
+		   ,@(reverse (fold-with-index (cute line 
+						     dir
+						     <>
+						     <>
+						     (+ (floor->exact (log (length entries) 10)) 1)
+						     make-url
+						     <>)
+					       (list) entries))
+		   ))))
 
 (provide "yogomacs/dired")
