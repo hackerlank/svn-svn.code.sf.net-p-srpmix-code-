6aac823b152a8950a873039e47b324e5a2d6f058
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-07-28 19:41:59 +0000

    *** empty log message ***

--- yogomacs/src/gauche/yogomacs/renderers/yogomacs.scm
+++ yogomacs/src/gauche/yogomacs/renderers/yogomacs.scm
@@ -59,8 +59,6 @@
 		    "-//W3C//DTD XHTML 1.0 Transitional//EN"
 		    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
 		    ) 
-	    "\n" 
-	    (*COMMENT* " Created by xhtmlize-1.34 in external-css mode. ")
 	    "\n"
 	    (html (|@| (xmlns "http://www.w3.org/1999/xhtml") (xml:lang "en") (lang "en"))
 		  "\n"
