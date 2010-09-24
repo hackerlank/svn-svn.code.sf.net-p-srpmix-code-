147ea93ebf7ba035957aeb93c2b7253a48d5e9d2
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-09-24 08:31:20 +0000

    Version 3.115.7

--- srpmix/trunk/autogen.sh
+++ srpmix/trunk/autogen.sh
@@ -9,7 +9,7 @@ aclocal -I misc/m4
 automake --add-missing --force-missing -Wno-portability
 autoconf
 
-REQUIRED="hardlink gauche emacs-common"
+# REQUIRED="hardlink gauche emacs-common"
 
 if [ -n "$1" ] && [ "$1" = --bootstrap] && which yum > /dev/null 2>&1; then
     sudo yum -y install ${REQUIRED}
