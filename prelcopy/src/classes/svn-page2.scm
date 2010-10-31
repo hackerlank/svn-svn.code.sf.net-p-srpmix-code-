7ea91c54e14b0a395175343a50e8b03da8cbb6e8
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2010-10-31 21:23:25 +0000

    *** empty log message ***

---
+++ prelcopy/src/classes/svn-page2.scm
@@ -0,0 +1,62 @@
+(use gauche.process)
+(use sxml.ssax)
+(use sxml.sxpath)
+(use syntax.htmlprag)
+(use srfi-1)
+
+(define jboss-svn-page2 "http://anonsvn.jboss.org/repos/svn-page2.html")
+(define inaccessible-projects '("teiiddesigner" "installer"))
+(define (svn-page2-top droppers)
+  (let1 sxml (call-with-input-process
+		 `(wget -q -O - ,jboss-svn-page2)
+	       (cute html->shtml <>))
+    (let1 urls ((sxpath `(// html body div div div div div table tr td a
+			     ,(lambda (node root vars)
+				((sxml:filter
+				  (lambda (node)
+				    (equal? (last node) "Anonymous")
+				    )) 
+				 node))
+			     @
+			     href
+			     *text*
+			     ))
+		sxml)
+      (map
+       (lambda (url)
+	 (cons (sys-basename url) url)
+	 )
+       (filter
+	(lambda (elt)
+	  (not (member (sys-basename elt) droppers)))
+	urls))
+      )))
+
+(define (svn-page2-project-dump url)
+  (let1 sxml (call-with-input-process
+		 `(wget -q -O - ,url)
+	       (cute html->shtml <>))
+    ((sxpath '(// html body ul li a *text*)) sxml)))
+
+(for-each
+   (lambda (top)
+     (let ((project (car top))
+	   (url (cdr top)))
+       (let1 dirs (svn-page2-project-dump url)
+	 (format #t "~a -> ~a\n" project url)
+	 (for-each 
+	  (lambda (dir)
+	    (when (#/\/$/ dir)
+	      (format #t "	~a -> ~a/~a\n" dir url dir)))
+	  dirs)
+	 )
+       ))
+   (svn-page2-top inaccessible-projects))
+    
+  
+;; ((branch "NAME" "SVN-SPEC-URL")...(tag "NAME" "SVN-SPEC-URL")...)
+(define (svn-page2-project url)
+  
+  )
+
+
