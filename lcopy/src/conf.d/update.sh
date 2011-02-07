b13724ed013e4d4261375b9ffb96949be7e76b4a
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2011-02-07 14:08:38 +0000

    Added more confs

--- lcopy/src/conf.d/update.sh
+++ lcopy/src/conf.d/update.sh
@@ -1,9 +1,12 @@
 export LANG=C
 dir=$1
+cd $dir
+class=$(basename $(pwd))
 lcopies=${dir}/*.lcopy
 
+{
 cat <<"EOF"
-confddir = $(sysconfdir)/lcopy/conf.d
+confddir = $(sysconfdir)/lcopy/conf.d/$class
 dist_confd_DATA = \
 	\
 EOF
@@ -14,3 +17,5 @@ for x in $lcopies; do
 done
 echo '	\'
 echo '	$(NULL)'
+
+} > Makefile.am
