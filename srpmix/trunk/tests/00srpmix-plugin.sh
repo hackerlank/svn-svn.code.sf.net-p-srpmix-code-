9459935c6e59f933945de7e58a5f72f6a7c9662a
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2011-04-11 18:29:01 +0000

    * src/srpmix-plugin: Report the status of plugin when 	listing.

--- srpmix/trunk/tests/00srpmix-plugin.sh
+++ srpmix/trunk/tests/00srpmix-plugin.sh
@@ -5,17 +5,17 @@ make DESTDIR=`pwd`/${TESTDIR} -C ${top_builddir} install
 
 PATH=`pwd`/${TESTDIR}/${bindir}:$PATH
 which srpmix-plugin
-diff <(srpmix-plugin --list | while read name arrow file; do echo $name; done) <(cat <<"EOF"
-coreutils
-cscope
-ctags
-doxygen
-etags
-file
-hyperestraier
-kindex
-nctags
-vanilla
-xgettext
+diff <(srpmix-plugin --list | while read status name arrow file; do echo $status $name; done) <(cat <<"EOF"
++ coreutils
++ cscope
++ ctags
++ doxygen
++ etags
++ file
+- hyperestraier
++ kindex
+- nctags
++ vanilla
++ xgettext
 EOF
 )
---
+++ srpmix/trunk/tests/01srpmix-plugin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+rm -rf `pwd`/${TESTDIR}
+make DESTDIR=`pwd`/${TESTDIR} -C ${top_builddir} install
+
+PATH=`pwd`/${TESTDIR}/${bindir}:$PATH
+which srpmix-plugin
+diff <(srpmix-plugin --disable=ALL --enable=file --list | while read status name arrow file; do echo $status $name; done) <(cat <<"EOF"
+- coreutils
+- cscope
+- ctags
+- doxygen
+- etags
++ file
+- hyperestraier
+- kindex
+- nctags
+- vanilla
+- xgettext
+EOF
+)
+
+
+diff <(srpmix-plugin --list --disable=ALL --enable=file | while read status name arrow file; do echo $status $name; done) <(cat <<"EOF"
+- coreutils
+- cscope
+- ctags
+- doxygen
+- etags
++ file
+- hyperestraier
+- kindex
+- nctags
+- vanilla
+- xgettext
+EOF
+)
