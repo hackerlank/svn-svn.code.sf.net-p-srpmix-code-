b9653c4c94f96c0f74f4ab270f4ba65fd1ba094d
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2011-04-28 17:50:42 +0000

    update

--- yogomacs/src/js/yogomacs-web.scm
+++ yogomacs/src/js/yogomacs-web.scm
@@ -41,33 +41,3 @@
 
 (define (sxml->xhtml sxml)
   (tree->string (sxml->xhtml0 sxml)))
-
-(define meta-variables (make-hashtable))
-(define (read-meta name)
-  (cond
-   ((hashtable-contains? meta-variables name)
-    (hashtable-get meta-variables name))
-   ((null? ($ name))
-    (let1 metas (vector->list ($$ "meta"))
-      (let loop ((metas metas))
-	(if (null? metas)
-	    #f
-	    (if (equal? (js-field (car metas) "name") name)
-		(let1 data (read-from-string (js-field (car metas) "content"))
-		  (begin
-		    (hashtable-put! meta-variables name data)
-		    data))
-		(loop (cdr metas)))))))
-   (else
-    (let1 elt ($ name)
-      (let* ((data (read-from-string elt.innerHTML)))
-	(hashtable-put! meta-variables name data)
-	(elt.remove)
-	data)))))
-
-(define (contents-url)
-  (let* ((location (js-field *js* "location"))
-	 (pathname (js-field location "pathname")))
-    (substring pathname
-	       (string-length shell-dir)
-	       (string-length pathname))))
