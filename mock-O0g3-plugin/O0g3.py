06b6e57e448e23bdd84e341188e5cdbd3879191f
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2011-08-05 19:34:10 +0000

    updated

--- mock-O0g3-plugin/O0g3.py
+++ mock-O0g3-plugin/O0g3.py
@@ -10,6 +10,8 @@ from mock.trace_decorator import decorate, traceLog, getLog
 import mock.util
 import shutil
 import os
+import os.path
+import glob
 
 requires_api_version = "1.0"
 
@@ -27,7 +29,10 @@ class O0g3(object):
         self.root = root
         self.opts = opts
         self.gcc_O0g3="/usr/share/mock-O0g3-plugin/gcc-O0g3"
-
+        self.suffix="O0g3"
+        
+        # See http://www.redhat.com/archives/rpm-list/2003-February/msg00174.html
+        root.yumInstall("redhat-rpm-config")
 
         root.addHook("prebuild",  self.prebuild)
         root.addHook("postbuild", self.postbuild)
@@ -47,7 +52,12 @@ class O0g3(object):
         spec = specs[0]
         chrootspec = spec.replace(root.makeChrootPath(), '') # get rid of rootdir prefix
         root.doChroot(
-            ["sed", "-i", "-e", "s/^Release: .*/\0.O0g3/", chrootspec],
+            ["sed", "-i", 
+             "-e", 's/^Release: .*/\\0.%s/'%(self.suffix), 
+             "-e", "1i %define debug_package %{nil}",
+             "-e", "1i %define debug_packages %{nil}",
+             "-e", "1i %define __strip :",
+             chrootspec],
             shell=False,
             logger=root.build_log, 
             timeout=0,
@@ -58,36 +68,40 @@ class O0g3(object):
     def replaceGcc(self):
         root = self.root
         f = open(root.makeChrootPath("/usr/bin/gcc"), mode='r')
-        if f.readline() != "#!/bin/bash":
-            shutil.copy(self.gcc_O0g3, root.makeChrootPath("/tmp"))
-            root.doChroot(
-                ["cp", "/usr/bin/gcc", "/usr/bin/_gcc"],
-                shell=False,
-                logger=root.build_log, 
-                timeout=0,
-                uid=root.chrootuid,
-                gid=root.chrootgid,
-                )
-            root.doChroot(
-                ["cp", "/tmp/gcc", "/usr/bin/gcc"],
-                shell=False,
-                logger=root.build_log, 
-                timeout=0,
-                uid=root.chrootuid,
-                gid=root.chrootgid,
-                )
-        close(f)
+        l = f.readline() 
+        f.close()
+        if l != "#!/bin/bash":
+            try:
+                root.uidManager.becomeUser(0, 0)
+                mock.util.do(
+                    ["/bin/cp", 
+                     root.makeChrootPath("/usr/bin/gcc"), 
+                     root.makeChrootPath("/usr/bin/_gcc")],
+                    shell=False)
+                mock.util.do(
+                    ["/bin/cp", 
+                     self.gcc_O0g3,
+                     root.makeChrootPath("/usr/bin/gcc")],
+                    shell=False)
+            finally:
+                root.uidManager.restorePrivs()
+    def revertGcc(self):
+        root = self.root
+        if os.path.exists(root.makeChrootPath("/usr/bin/_gcc")):
 
+            try:
+                root.uidManager.becomeUser(0, 0)
+                mock.util.do(
+                    ["/bin/mv", 
+                     root.makeChrootPath("/usr/bin/_gcc"), 
+                     root.makeChrootPath("/usr/bin/gcc")],
+                    shell=False)
+            finally:
+                root.uidManager.restorePrivs()
+        
     decorate(traceLog())
     def postbuild(self):
-        root = self.root
         getLog().info("Revert gcc")
-        root.doChroot(
-                ["mv", "/usr/bin/_gcc", "/usr/bin/gcc"],
-                shell=False,
-                logger=root.build_log, 
-                timeout=0,
-                uid=root.chrootuid,
-                gid=root.chrootgid,
-                )
-# mock --resultdir=/tmp --enable-plugin=O0g3 -r epel-5-x86_64 --rebuild /srv/sources/attic/cradles/ftp.redhat.com/mirror/linux/enterprise/5Server/en/os/SRPMS/device-mapper-multipath-0.4.7-46.el5.src.rpm
+        self.revertGcc()
+
+# mock --no-cleanup-after --resultdir=/tmp --enable-plugin=O0g3 -r epel-5-x86_64 --rebuild /srv/sources/attic/cradles/ftp.redhat.com/mirror/linux/enterprise/5Server/en/os/SRPMS/device-mapper-multipath-0.4.7-46.el5.src.rpm 
