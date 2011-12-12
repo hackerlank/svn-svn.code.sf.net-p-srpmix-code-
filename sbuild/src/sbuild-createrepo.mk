cfdea74fd9aa0a89f226dc339712a3bf019eb846
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2011-12-12 05:25:19 +0000

    updated

--- sbuild/src/sbuild-createrepo.mk
+++ sbuild/src/sbuild-createrepo.mk
@@ -3,8 +3,9 @@
 #RPMS := $(shell find -name '*.rpm')
 RPMS := $(wildcard [0-9a-zA-Z]) $(wildcard weakview) $(wildcard dir-pkg/[0-9a-zA-Z]) $(wildcard *.rpm)
 CREATEREPO_OPTS =
+COMPS := $(shell test -f comps.xml && echo "comps.xml")
+COMPS_OPT := $(if $(COMPS),-g comps.xml,)
 
 all: repodata/primary.xml.gz
-repodata/primary.xml.gz: $(RPMS)
-	if test -f comps.xml; then COMPS_OPT="-g comps.xml"; fi; \
-		createrepo $(CREATEREPO_OPTS) -d --skip-stat $$COMPS_OPT  .
+repodata/primary.xml.gz: $(RPMS) $(COMPS)
+	createrepo $(CREATEREPO_OPTS) -d --skip-stat $(COMPS_OPT) .
