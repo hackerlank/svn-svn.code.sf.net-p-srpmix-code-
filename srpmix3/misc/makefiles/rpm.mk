8a5a05527dfcab5c0497caa414087560498a999c
m_yamato <m_yamato@e71b4c53-ea4f-0410-8961-bf8a5b3cd2e8>
2014-05-15 11:21:11 +0000

    Added srpmix-weakview-repo

--- srpmix3/misc/makefiles/rpm.mk
+++ srpmix3/misc/makefiles/rpm.mk
@@ -13,8 +13,17 @@ srpm: distcheck xspecs
 rpm: distcheck xspecs
 	$(mkinstalldirs) build/{SPECS,RPMS,BUILD,SRPMS}
 	rpmbuild --define "_topdir `pwd`/build" -ta $(DIST_ARCHIVES)
+
+#       SPEC->sepc:
+#	The original spec file has SPEC(upper case) as suffix. src.rpm including such
+#	spec file is rejected by mock. To embed spec file which has spec(lower case)
+#	as suffix, use temporary spec file.
+
 	for x in $(XSPECS); do                               \
-		rpmbuild --define "_topdir `pwd`/build" -ba $(XSPEC_INPUT_PREFIX)$${x}; \
+		y=$$(echo $${x} | tr 'SPEC' 'spec');         \
+		cp $(XSPEC_INPUT_PREFIX)$${x} $(XSPEC_INPUT_PREFIX)$${y}; \
+		rpmbuild --define "_topdir `pwd`/build" -ba $(XSPEC_INPUT_PREFIX)$${y}; \
+		rm $(XSPEC_INPUT_PREFIX)$${y}; \
 	done
 
 clean-local::
